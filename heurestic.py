3000d9f53f9684b04e4f474640ce8fe9b40b7cf8
Ryan <53787709+ryanding1@users.noreply.github.com>
2020-11-11 17:58:22 -0700

    Update heurestic.py
    pawn diagonal checker implemented in half heuristic

--- heurestic.py
+++ heurestic.py
@@ -1,5 +1,7 @@
 import chess
 def halfheurestic(board): #should run the heurestic from whites percpective and look primarly at white's things(like only white's material and stuff)
+    heuristicscore = 0
+    #grid setup to represent the board
     grid = [['' for i in range(8)] for j in range(8)]
     for y in range(8):
         for x in range(8):
@@ -8,7 +10,26 @@ def halfheurestic(board): #should run the heurestic from whites percpective and 
                 grid[x][y] = ' '
             else:
                 grid[x][y] = piece.symbol()
-    return 0
+
+    #checks if pawns are diagonal to each other
+    for y in range(8):
+        for x in range(8):
+            if (grid[x][y] == 'P'):
+                if (x - 1 >= 0):
+                    if (y + 1 <= 7):
+                        if(grid[x-1][y+1] == 'P'):
+                            heuristicscore += 1
+                    if (y - 1 >= 0):
+                        if(grid[x-1][y-1] == 'P'):
+                            heuristicscore += 1
+                if (x + 1 <= 7):
+                    if (y + 1 <= 7):
+                        if(grid[x+1][y+1] == 'P'):
+                            heuristicscore += 1
+                    if (y - 1 >= 0):
+                        if(grid[x+1][y-1] == 'P'):
+                            heuristicscore += 1
+    return heuristicscore
 
 def heurestic(board, side): #side is the side "you" are playing as in the heurestic. The half heurestic is run for both sides and the difference is the final output
     if (board.is_game_over()):
